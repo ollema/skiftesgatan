891b7253630fcdfada96285a53613803194bffbc
Olle Månsson <31876997+ollema@users.noreply.github.com>
2022-11-19 19:04:26 +0100

    inital work on content delivery

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
+<h3>BRF Skiftesgatan 4</h3>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
---
+++ src/routes/nyheter/[slug]/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta data-key="description" name="description" content={data.meta.description} />
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+	<meta name="twitter:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
+	<meta name="twitter:description" content={data.meta.description} />
+</svelte:head>
+
+<h3 class="title">{data.meta.title}</h3>
+<div class="meta">{data.meta.date}</div>
+{@html data.content}
+
+<style lang="postcss">
+	.title {
+		margin-bottom: var(--size-3);
+	}
+
+	.meta {
+		color: var(--text-2);
+		font-size: var(--font-size);
+
+		margin-bottom: var(--size-2);
+	}
+</style>
