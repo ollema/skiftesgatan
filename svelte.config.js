b251a5e9f789a29fe00db5214c7a9e7645bd2e1c
Olle Månsson <31876997+ollema@users.noreply.github.com>
2022-11-19 11:49:41 +0100

    change to static adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,21 @@
+import adapter from '@sveltejs/adapter-static';
+import preprocess from 'svelte-preprocess';
+
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
-import adapter from '@sveltejs/adapter-cloudflare';
-import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	extensions: ['.svelte', ...mdsvexConfig.extensions],
-	preprocess: [
-		preprocess({
-			postcss: true
-		}),
-		mdsvex(mdsvexConfig)
-	],
-
+	preprocess: [preprocess({postcss: true}), mdsvex(mdsvexConfig)],
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false,
+      strict: true
+    })
 	}
 };
 
