92a61e9c01764499b9f567bbbede494d0111ee96
Olle Månsson <31876997+ollema@users.noreply.github.com>
2023-11-13 12:35:46 +0100

    adjust cookie settings

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,22 +4,18 @@ export async function handle({ event, resolve }) {
 	event.locals.pb = new PocketBase('https://pocketbase.skiftesgatan.com');
 	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
-	try {
-		event.locals.pb.authStore.isValid && (await event.locals.pb.collection('users').authRefresh());
-		const model = event.locals.pb.authStore.model;
-		if (model) {
+	if (event.locals.pb.authStore.isValid) {
+		const record = event.locals.pb.authStore.model;
+		if (record) {
 			event.locals.user = {
-				id: model.id,
-				username: model.username,
-				name: model.name,
-				avatar: model.avatar
+				id: record.id,
+				username: record.username,
+				name: record.name,
+				avatar: record.avatar
 			};
 		}
-	} catch (_) {
-		event.locals.pb.authStore.clear();
 	}
 
 	const response = await resolve(event);
-	response.headers.append('set-cookie', event.locals.pb.authStore.exportToCookie());
 	return response;
 }
