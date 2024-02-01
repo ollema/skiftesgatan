4c7072d29269228487a5f359f50e4e06cdfcaeed
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-02-01 17:39:08 +0100

    improve mobile experience

--- src/lib/components/site-header.svelte
+++ src/lib/components/site-header.svelte
@@ -3,7 +3,7 @@
 	import { MainNav, MobileNav } from '$lib/components/navigation';
 </script>
 
-<header class="sticky top-0 z-40 w-full border-b-2 bg-banner">
+<header class="pt-safe sticky top-0 z-40 w-full border-b-2 bg-banner">
 	<div class="container flex h-14 items-center">
 		<a
 			class="mr-6 flex flex-shrink-0 items-center text-2xl font-black text-banner-foreground hover:underline"
