2245195a203ea8b67ad5befd310eefc9845097be
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-02-03 02:36:08 +0100

    maybe fix login bug

--- src/lib/pocketbase/client.ts
+++ src/lib/pocketbase/client.ts
@@ -1,13 +1,65 @@
-import PocketBase, { AsyncAuthStore } from 'pocketbase';
+import PocketBase, { BaseAuthStore, type AuthModel } from 'pocketbase';
 import type { TypedPocketBase } from '$lib/pocketbase-types';
 import { Preferences } from '@capacitor/preferences';
 
+class PreferencesAuthStore extends BaseAuthStore {
+	private queue: Array<() => Promise<void>> = [];
+
+	constructor() {
+		super();
+		this._enqueue(() => this._loadInitial());
+	}
+
+	save(token: string, model?: AuthModel): void {
+		super.save(token, model);
+		this._enqueue(
+			async () => await Preferences.set({ key: authKey, value: JSON.stringify({ token, model }) })
+		);
+	}
+
+	clear(): void {
+		super.clear();
+		this._enqueue(async () => await Preferences.remove({ key: authKey }));
+	}
+
+	private async _loadInitial() {
+		try {
+			const result = await Preferences.get({ key: authKey });
+			const parsed = JSON.parse(result.value || '{}') || {};
+			this.save(parsed.token || '', parsed.model || null);
+		} catch (_) {
+			// do nothing
+		}
+	}
+
+	private _enqueue(asyncCallback: () => Promise<void>) {
+		this.queue.push(asyncCallback);
+
+		if (this.queue.length == 1) {
+			this._dequeue();
+		}
+	}
+
+	private _dequeue() {
+		if (!this.queue.length) {
+			return;
+		}
+
+		this.queue[0]().finally(() => {
+			this.queue.shift();
+
+			if (!this.queue.length) {
+				return;
+			}
+
+			this._dequeue();
+		});
+	}
+}
+
 const authKey = 'pb_auth';
 
-const store = new AsyncAuthStore({
-	initial: Preferences.get({ key: authKey }),
-	save: async (serialized) => await Preferences.set({ key: authKey, value: serialized }),
-	clear: async () => await Preferences.remove({ key: authKey })
-});
-
-export const pb = new PocketBase('https://skiftesgatan.com', store) as TypedPocketBase;
+export const pb = new PocketBase(
+	'https://skiftesgatan.com',
+	new PreferencesAuthStore()
+) as TypedPocketBase;
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,10 +1,14 @@
 import { pb, maybeGetApartmentForUser } from '$lib/pocketbase';
+import { Preferences } from '@capacitor/preferences';
 
 export const ssr = false;
 export const prerender = false;
 export const trailingSlash = 'always';
 
 export const load = async ({ fetch }) => {
+	// TODO: figure out if there is a better way to wait for the auth store to be fully loaded
+	await Preferences.get({ key: 'pb_auth' });
+
 	if (!pb.authStore.isValid) {
 		pb.authStore.clear();
 	}
