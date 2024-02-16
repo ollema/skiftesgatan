922b976f545232e5f068d96a15d503eb43dcf167
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-02-16 22:28:41 +0100

    split app into two adapters

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,35 @@
+import adapterStatic from '@sveltejs/adapter-static';
+import adapterNode from '@sveltejs/adapter-node';
+
 import sequence from 'svelte-sequential-preprocessor';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { preprocessMeltUI } from '@melt-ui/pp';
-import adapter from '@sveltejs/adapter-static';
+
+const adapter = process.env.ADAPTER === 'node' ? adapterNode : adapterStatic;
+const adapterConfig =
+	process.env.ADAPTER === 'node'
+		? {
+				out: 'build-node'
+			}
+		: {
+				pages: 'build-static',
+				assets: 'build-static',
+				fallback: 'index.html',
+				precompress: false,
+				strict: true
+			};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: sequence([vitePreprocess({}), preprocessMeltUI()]),
 	kit: {
-		adapter: adapter({
-			pages: 'pb_public',
-			assets: 'pb_public',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
+		adapter: adapter(adapterConfig),
 		csrf: {
 			checkOrigin: process.env.NODE_ENV === 'development' ? false : true
 		},
 		serviceWorker: {
 			register: false
 		}
-	},
-	vitePlugin: {
-		inspector: true
 	}
 };
 
