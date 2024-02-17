0ccc10218464af015e7dfbfd3fd6771e73736e39
Olle Månsson <31876997+ollema@users.noreply.github.com>
2024-02-17 04:04:05 +0100

    add missing return

--- src/routes/apartments/[apartment]/+page.server.ts
+++ src/routes/apartments/[apartment]/+page.server.ts
@@ -3,5 +3,5 @@ import { loadData } from './load';
 export const load = async ({ parent, locals, params, fetch }) => {
 	await parent();
 
-	loadData(locals.pb, params.apartment, fetch);
+	return loadData(locals.pb, params.apartment, fetch);
 };
