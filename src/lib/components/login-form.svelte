2c3f4e90458e63ea586327426688f0d68487a56b
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-06-02 11:56:50 +0200

    add basic login form

---
+++ src/lib/components/login-form.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
+	import { Input } from '$lib/components/ui/input/index.js';
+	import { Label } from '$lib/components/ui/label/index.js';
+</script>
+
+<Card.Root class="mx-auto w-full max-w-sm">
+	<Card.Header>
+		<Card.Title class="text-2xl">Logga in</Card.Title>
+		<Card.Description>Ange din email och lösenord för att logga in</Card.Description>
+	</Card.Header>
+	<Card.Content>
+		<div class="grid gap-4">
+			<div class="grid gap-2">
+				<Label for="email">Email</Label>
+				<Input id="email" type="email" placeholder="m@example.com" required />
+			</div>
+			<div class="grid gap-2">
+				<div class="flex items-center">
+					<Label for="password">Lösenord</Label>
+					<a href="##" class="ml-auto inline-block text-sm underline">Glömt ditt lösenord?</a>
+				</div>
+				<Input id="password" type="password" required />
+			</div>
+			<Button type="submit" class="w-full">Logga in</Button>
+		</div>
+		<div class="mt-4 text-center text-sm">
+			Har du inte ett konto?
+			<a href="##" class="underline">Registrera dig</a>
+		</div>
+	</Card.Content>
+</Card.Root>
