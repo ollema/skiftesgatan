a0b95438f38124b1f5a2e1db841360b1180f6ef1
Olle Månsson <31876997+ollema@users.noreply.github.com>
2025-06-05 11:52:44 +0200

    feat: add load function to retrieve user from event locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,11 @@
-// for information about these interfaces
 declare global {
 	namespace App {
 		interface Locals {
 			user: import('$lib/server/auth/session').SessionValidationResult['user'];
 			session: import('$lib/server/auth/session').SessionValidationResult['session'];
+		}
+		interface PageData {
+			user: import('$lib/server/auth/session').SessionValidationResult['user'];
 		}
 	}
 }
